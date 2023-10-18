c058caab98a8f041f97a3c7aeed9307f867c5f86
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2023-10-18 17:05:21 -0500

    Update validations.py

--- UI/validations.py
+++ UI/validations.py
@@ -1,1 +1,18 @@
+import re
 
+regex_cedula = '^[0-9]{8,10}$'
+regex_nombre = '^((\w+\s)*\w+){3,50}$'
+
+
+def validar_cedula(cedula):
+    if not re.search(regex_cedula, cedula):
+        return False
+    else:
+        return True
+
+
+def validad_nombre(nombre):
+    if not re.search(regex_nombre, nombre):
+        return False
+    else:
+        return True
