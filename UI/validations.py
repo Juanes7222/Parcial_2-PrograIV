55686112a99e9de574429353389ca5396692126c
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2023-10-18 23:26:03 -0500

    Update validations.py

--- UI/validations.py
+++ UI/validations.py
@@ -1,18 +1,18 @@
 import re
 
 regex_cedula = '^[0-9]{8,10}$'
-regex_nombre = '^((\w+\s)*\w+){3,50}$'
+regex_name = '^((\w+\s)*\w+){3,50}$'
 
 
-def validar_cedula(cedula):
+def validate_cedula(cedula):
     if not re.search(regex_cedula, cedula):
         return False
     else:
         return True
 
 
-def validad_nombre(nombre):
-    if not re.search(regex_nombre, nombre):
+def validate_name(name):
+    if not re.search(regex_name, name):
         return False
     else:
         return True
