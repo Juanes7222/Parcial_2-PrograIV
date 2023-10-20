901ccc02e40bc7dbcc0328a63fc901a19e67b691
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2023-10-19 23:23:39 -0500

    Update menu.py

--- UI/menu.py
+++ UI/menu.py
@@ -3,6 +3,7 @@ from .mensajes import show_errors as show_error
 from .validations import validate_cedula
 from .validations import validate_name
 
+
 def show_options():
     header("BIENVENIDO A NUESTRO NEGOCIO")
     print("1. Hacer nueva compra")
@@ -15,7 +16,7 @@ def show_options():
 def stock_product(product):
     while True:
         numeration = 1
-        header("ARTÍCULOS A LA VENTA")  
+        header("ARTÍCULOS A LA VENTA")
         for name, info in product.items():
             print(f"{numeration}. {name} ", end="")
             for arg, value in info.items():
@@ -30,14 +31,16 @@ def stock_product(product):
 
 
 def main_menu():
-    while True: 
+    while True:
         show_options()
-        command = int(input("¿Qué desea hacer?: "))
-        # os.system("cls")
-        if command > 4 or command < 1:
+        try:
+            command = int(input("¿Qué desea hacer?: "))
+            if command > 4 or command < 1:
+                raise ValueError
+            else:
+                return command
+        except ValueError:
             show_error(1)
-        else:
-            return command
 
 
 def user_info_ingress():
@@ -47,18 +50,19 @@ def user_info_ingress():
         if validate_cedula(dni):
             return dni
         show_error(2)
-    
+
+
 def user_ingress_name():
-    name = input("\nNombre completo (mínimo 3 caracteres y máximo 50): ").lower()
     while True:
+        name = input("\nNombre completo (mínimo 3 caracteres y máximo 50): ").lower()
         if validate_name(name):
             return name
         show_error(3)
 
+
 def show_purchase_options(__case=1):
     while True:
         header("ARTÍCULOS A LA VENTA")
-        # print("Total = " + str(total_cost))
         print("1. Antibióticos")
         print("2. Fertilizantes")
         print("3. Control de plagas")
@@ -67,7 +71,8 @@ def show_purchase_options(__case=1):
         if 1 <= command <= 3:
             return command
         show_error(1)
-        
+
+
 def get_params(params):
     header("Ingrese los valores")
     values = {}
@@ -75,22 +80,23 @@ def get_params(params):
         value = input(f"{param}: ")
         values[param] = value
     return values
-            
+
+
 def show_info_products(products):
     for product in products:
         print(product, '\n')
-        
-    
+
+
 def show_info_bills(bills):
     for number, bill in enumerate(bills, 1):
         header(f"FACTURA #{number} - Fecha: {bill.date}:")
         show_info_products(bill.objects)
         header(f"VALOR TOTAL: {bill.total_value()}")
-        
+
+
 def show_info_client(client):
     if isinstance(client, bool):
         show_error(4)
     else:
         header(f"BÚSQUEDA POR CÉDULA - CLIENTE: {client.name.title()}  CEDULA: {client.dni}")
         show_info_bills(client.bills)
-        
